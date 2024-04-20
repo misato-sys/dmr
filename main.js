a3810f19c9e2f3432260752127c7d47072362a92
misato-sys <167591707+misato-sys@users.noreply.github.com>
2024-04-20 10:32:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -100,3 +100,46 @@ const quiz = [
  correct: '17歳'
  },
 ]
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+ 
+const button = document.getElementsByTagName('button');
+const buttonLength = button.length;
+ 
+const setupQuiz = () => {
+ document.getElementById('question').textContent = quiz[quizIndex].question;
+ let buttonIndex = 0;
+ while(buttonIndex < buttonLength) {
+　　　　 button[buttonIndex].textContent = quiz[quizIndex].choices[buttonIndex];
+　　　　 buttonIndex++;
+ }
+}
+ 
+setupQuiz();
+
+const clickHandler = (e) => {
+ if (quiz[quizIndex].correct === e.target.textContent) {
+   window.alert("正解！");
+   score++;
+ } else {
+   window.alert("不正解！");
+ }
+ 
+ quizIndex++;
+  if (quizIndex < quizLength) {
+    setupQuiz();
+ } else {
+  window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+ }
+}
+ 
+let handlerIndex = 0;
+while(handlerIndex < buttonLength) {
+ button[handlerIndex].addEventListener('click', (e) => {
+   clickHandler(e);
+ });
+ handlerIndex++;
+ 
+}
