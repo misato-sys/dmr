2ceb4cf1288c266cce2ee89301a060e69865e1e4
misato-sys <167591707+misato-sys@users.noreply.github.com>
2024-04-20 15:50:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -813,7 +813,7 @@ const setupQuiz = () => {
 setupQuiz();
 
 ver miss=new Array();
-let m_index=0;
+/*let m_index=0;*/
 
 const clickHandler = (e) => {
  if (quiz[quizIndex].correct === e.target.textContent) {
@@ -821,8 +821,8 @@ const clickHandler = (e) => {
    score++;
  } else {
    
-   miss[m_index]=quizIndex;
-   m_index++;
+   /*miss[m_index]=quizIndex;*/
+   /*m_index++;*/
    window.alert("不正解！:" + quizindex);
  }
  
